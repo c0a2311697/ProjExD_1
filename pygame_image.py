2905eec05196aa172adeb937b1cf1de7bffcc5ef
C0A23116 <c0a2311697@edu.teu.ac.jp>
2024-06-11 16:35:51 +0900

    背景のループ解消

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.transform.flip(bg_img, True, False)
     kouka3_img = pg.image.load("fig/3.png")
     kouka3_img = pg.transform.flip(kouka3_img, True, False)
 
@@ -19,15 +20,19 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
-        x = tmr % 800
+        x = tmr % 3200
         screen.blit(bg_img, [-x, 0])
+        screen.blit(bg_img2, [-x+1600, 0])
+        screen.blit(bg_img, [-x+3200, 0])
+        screen.blit(bg_img2, [-x+4800, 0])
+
 
         kouka3_rct = kouka3_img.get_rect()
         kouka3_rct.center = 300,200
         screen.blit(kouka3_img, kouka3_rct)
         pg.display.update()
         tmr += 1        
-        clock.tick(100)
+        clock.tick(500)
 
 
 if __name__ == "__main__":
